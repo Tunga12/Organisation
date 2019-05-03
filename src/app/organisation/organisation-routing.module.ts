ce5034c228b393cfadab3990d43e2989ab787973
Tunga Tessema <tungat72@gmail.com>
2019-05-03 09:07:40 +0300

    Added a layout

--- src/app/organisation/organisation-routing.module.ts
+++ src/app/organisation/organisation-routing.module.ts
@@ -5,14 +5,27 @@ import { OrganisationFormComponent } from './organisation-form/organisation-form
 import { OrganisationComponent } from './organisation.component';
 
 const routes: Routes = [
-    {path: '', redirectTo: '/organisations', pathMatch: 'full' },
-    {path: 'organisations', component: OrganisationComponent},
-  {path: 'organisations/new', component: OrganisationFormComponent},
-    {path: 'organisations/:id/edit', component: OrganisationFormComponent},
+  { path: '', redirectTo: '/organisations', pathMatch: 'full' },
+  {
+    path: 'organisations', component: OrganisationComponent, data: {
+      breadcrumb: 'Organisation List'
+    }
+  },
+  {
+    path: 'organisations/new', component: OrganisationFormComponent, data: {
+      breadcrumb: 'Add Organisation'
+    }
+  },
+  {
+    path: 'organisations/:id/edit', component: OrganisationFormComponent, data: {
+      breadcrumb: 'Edit Organisation'
+    }
+  },
 ]
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class OrganisationRoutingModule { }
+
